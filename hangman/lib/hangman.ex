8cf043682851751523a3116484f900dc05267751
Richard Van Camp <richard.vancamp@gofore.com>
2017-06-28 18:54:53 +0300

    basic hangman working

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -3,5 +3,11 @@ defmodule Hangman do
   alias Hangman.Game, as: HM
 
   defdelegate new_game(), to: HM
+  defdelegate tally(game), to: HM
+
+  def guess(game, guess) do
+    game = HM.guess(game, guess)
+    {game, tally(game)}
+  end
 
 end
